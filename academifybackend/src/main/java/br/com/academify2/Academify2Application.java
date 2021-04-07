18e26dc0e4ab2d9b10f114d5e3893ed485fa6952
Victor Vidigal Ribeiro <victorvidigal@gmail.com>
2021-04-07 20:09:38 -0300

    Primeiro commit

---
+++ academifybackend/src/main/java/br/com/academify2/Academify2Application.java
@@ -0,0 +1,13 @@
+package br.com.academify2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Academify2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Academify2Application.class, args);
+    }
+
+}
---
+++ academifybackend/src/test/java/br/com/academify2/Academify2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.academify2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Academify2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
