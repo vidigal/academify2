a564f63a850cec63eceb8ef8411a485629ce279c
Victor Vidigal Ribeiro <victorvidigal@gmail.com>
2021-04-13 20:03:48 -0300

    Criação da classe Aluno

---
+++ academifybackend/src/main/java/br/com/academify2/model/Aluno.java
@@ -0,0 +1,29 @@
+package br.com.academify2.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="aluno")
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String nome;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
